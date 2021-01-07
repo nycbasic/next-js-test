4e0ebddc69f76eb6ad36968069882f5ed1fd2c73
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-01-07 08:46:50 -0500

    fetching posts with getIntialProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import App, { Container } from "next/app";
+import App from "next/app"
 import NavBar from "../components/Navbar";
+import axios from "axios"
 
-class MyApp extends App {
-    static async getInitialProps({Component, ctx}) {
-        let pageProps = {};
-
-        if(Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-
-        return {pageProps};
-    }
-
-    render() {
-        const { Component, pageProps } = this.props;
-        return (
-            <Container>
-                <NavBar />
-                <Component {...pageProps} />
-            </Container>
-        )
-    }
+const MyApp = ({Component, pageProps }) => {
+    return (
+        <React.Fragment>
+            <NavBar />
+            <Component {...pageProps} />
+        </React.Fragment>
+    )
 }
+
 
 export default MyApp;
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
-import Link from "next/link";
-import NavBar from "../components/Navbar";
+import axios from "axios";
 
-const Index = () => {
+const Index = ({posts}) => {
     console.log("HOME")
     return (
         <div>
             <h1>OUR INDEX PAGE</h1>
+            <ul>
+               {posts.map(post => {
+                   return <li key={post.id}>{post.title}</li>
+                })} 
+            </ul>
+            
         </div>
     )
+}
+
+Index.getInitialProps = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const {data} = res;
+    return {
+        posts: data
+    }
 }
 
 export default Index;
