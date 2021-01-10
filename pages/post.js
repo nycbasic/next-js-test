3ef077d53775eb7f90656b2813349af1118f8478
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-01-09 19:12:11 -0500

    fetching comments

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,28 @@
 import { withRouter } from "next/router";
+import axios from "axios";
 
 const Post = (props) => {
     console.log(props);
     return (
-        <h1>{`You are looking at post: ${props.router.query.id}`}</h1>
-    )
+        <div>
+            <h1>{`Comments for post: ${props.id}`}</h1>
+            {props.comments.map(comment =>{
+                const {name, email, body} = comment;
+                return (
+                    <div>
+                        <h3>{name}</h3>
+                        <p>{email}</p>
+                        <p>{body}</p>
+                    </div>
+                )
+            })}
+        </div>
+      )   
 }
 
-// Post.getInitialProps = async ({query}) => {
-//     console.log(query)
-//     return query;
-// }
-export default withRouter(Post);
+Post.getInitialProps = async ({query}) => {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${query.id}`)
+    const {data} = res;
+    return {...query, comments: data};
+}
+export default Post;
